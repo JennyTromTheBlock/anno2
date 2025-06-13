afab27df0c326476bbf1a68c821ad626ff963531
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2025-06-13 09:41:06 +0200

    isloation of search temp

---
+++ search_service/Presentation/controllers/IndexFilesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IndexFilesController : ControllerBase
+    {
+        public IndexFilesController()
+        {
+        }
+
+        [HttpPost]
+        public IActionResult IndexFile([FromBody] string filePath)
+        {
+            // TODO: Call application service to index file
+            return Ok($"File at '{filePath}' indexed.");
+        }
+    }
+}
